edc9014344b58f1a539fde5ff4905f4f39a6754a
Robert Sigler <rsigler@gmail.com>
2012-08-24 16:44:24 +0000

    fixed up elevation profile data

--- Util.php
+++ Util.php
@@ -1,5 +1,7 @@
 <?php
 class Util {
+		const feetPerMeter = 3.2808399;
+
 	static function eol() {
 		switch (php_sapi_name()) {
 			case "cli": 
@@ -10,6 +12,14 @@ class Util {
 				break;
 		}
 		return $eol;
+	}
+
+	static function meters2feet($meters) {
+		return $meters * self::feetPerMeter;
+	}
+
+	static function seconds2HumanTime($secs) {
+		return array ("hours" => intval($secs / 3660), "minutes" => intval(($secs / 60) % 60), "seconds" => intval($secs % 60));
 	}
 }
 ?>
--- elevation.php
+++ elevation.php
@@ -4,11 +4,26 @@ require_once("lib.php");
 if (isset($_GET["trackID"])) {
 	$trackID = $_GET["trackID"];
 	$elevationProfile = "<img src=\"eleProfile.php?trackID=$trackID\">";
-	$req = $db->query("SELECT trackName, trackDate, userDescr FROM track_tables_info WHERE ID = $trackID");
+	$req = $db->query("SELECT tableName, trackName, trackDate, userDescr FROM track_tables_info WHERE ID = $trackID");
+	$tableName = $req["rows"][0]["tableName"];
 	$trackName = $req["rows"][0]["trackName"]; 
 	$trackDate = $req["rows"][0]["trackDate"]; 
 	$trackDescr = $req["rows"][0]["userDescr"]; 
-	$info = "Track: $trackName<br>Date: $trackDate<br>Descr: $trackDescr<br>";
+	$info = "Track: $trackName<br>";
+	$info .= "Date: $trackDate<br>";
+	$info .= "Descr: $trackDescr<br>";
+
+	$req = $db->query("SELECT MAX(ele) as maxElev, MIN(ele) as minElev, TIMESTAMPDIFF(SECOND, MIN(time), MAX(time)) as timeDiff FROM $tableName");
+	$maxElev = $req["rows"][0]["maxElev"];
+	$minElev = $req["rows"][0]["minElev"];
+	$timeDiff = $req["rows"][0]["timeDiff"];
+
+	$info .= "Min Elevation: " . round($minElev, 2) . " meters / " . round(Util::meters2feet($minElev), 2) . " feet<br>";
+	$info .= "Max Elevation: " . round($maxElev, 2) . " meters / " . round(Util::meters2feet($maxElev), 2) . " feet<br>";
+	$info .= "Elevation Change: " . round(($maxElev - $minElev), 2) . " meters / " . round(Util::meters2feet($maxElev - $minElev), 2) . " feet<br>";
+	$duration = Util::seconds2HumanTime($timeDiff);
+	$info .= sprintf("Hike duration %02d:%02d:%02d<br>", $duration["hours"], $duration["minutes"], $duration["seconds"]);
+
 } else {
 	header("Location: index.php");
 }
